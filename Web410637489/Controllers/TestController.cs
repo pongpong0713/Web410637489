788d5f43e3ee5b06fa59f164ddb726a52ce8db1c
蕭文芃 <spexial10010713@gmail.com>
2021-04-29 20:23:59 +0800

    4/29

---
+++ Web410637489/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410637489.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
